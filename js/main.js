0d23dacf5989f24ee4211244c2e3448de2eece17
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-06-30 23:14:35 +0630

    #add: hide-menu when link is clicked js

--- js/main.js
+++ js/main.js
@@ -3,16 +3,25 @@ const navMenu = document.getElementById("nav-menu");
 const navToggle = document.getElementById("nav-toggle");
 const navClose = document.getElementById("nav-close");
 
-// Show Menu
+// Show Menu when toggle button is clicked
 if (navToggle) {
     navToggle.addEventListener("click", () => {
         navMenu.classList.add("show-menu");
     });
 }
 
-// hide menu
+// hide menu when close button is clicked
 if (navClose) {
     navClose.addEventListener("click", () => {
         navMenu.classList.remove("show-menu");
     });
 }
+
+// hide menu when each link is clicked
+const navLinks = document.querySelectorAll(".nav__link");
+
+function removeMenu() {
+    const navMenu = document.getElementById("nav-menu");
+    navMenu.classList.remove("show-menu");
+}
+navLinks.forEach((navLink) => navLink.addEventListener("click", removeMenu));
